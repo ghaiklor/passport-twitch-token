f5bdc75d28cc4c977680a6460a0785f188fa936f
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-01 13:55:48 +0300

    Rewrite strategy with ES6

---
+++ src/index.js
@@ -0,0 +1,121 @@
+import { OAuth2Strategy, InternalOAuthError } from 'passport-oauth';
+
+/**
+ * `Strategy` constructor.
+ * The Twitch authentication strategy authenticates requests by delegating to Twitch using OAuth2 access tokens.
+ * Applications must supply a `verify` callback which accepts a accessToken, refreshToken, profile and callback.
+ * Callback supplying a `user`, which should be set to `false` if the credentials are not valid.
+ * If an exception occurs, `error` should be set.
+ *
+ * Options:
+ * - clientID          Identifies client to Twitch App
+ * - clientSecret      Secret used to establish ownership of the consumer key
+ * - passReqToCallback If need, pass req to verify callback
+ *
+ * Example:
+ *     passport.use(new TwitchTokenStrategy({
+ *           clientID: '123-456-789',
+ *           clientSecret: 'shhh-its-a-secret',
+ *           passReqToCallback: true
+ *       }, function(req, accessToken, refreshToken, profile, next) {
+ *              User.findOrCreate(..., function (error, user) {
+ *                  next(error, user);
+ *              });
+ *          }
+ *       ));
+ *
+ * @param {Object} _options
+ * @param {Function} _verify
+ * @constructor
+ */
+export default class TwitchTokenStrategy extends OAuth2Strategy {
+  constructor(_options, _verify) {
+    let options = _options || {};
+    let verify = _verify;
+
+    options.authorizationURL = options.authorizationURL || 'https://api.twitch.tv/kraken/oauth2/authorize';
+    options.tokenURL = options.tokenURL || 'https://api.twitch.tv/kraken/oauth2/token';
+
+    super(options, verify);
+
+    this.name = 'twitch-token';
+    this._accessTokenField = options.accessTokenField || 'access_token';
+    this._refreshTokenField = options.refreshTokenField || 'refresh_token';
+    this._profileURL = options.profileURL || 'https://api.twitch.tv/kraken/user';
+    this._passReqToCallback = options.passReqToCallback;
+    this._oauth2.setAuthMethod('OAuth');
+    this._oauth2.useAuthorizationHeaderforGET(true);
+  }
+
+  /**
+   * Authenticate method
+   * @param {Object} req
+   * @param {Object} options
+   * @returns {*}
+   */
+  authenticate(req, options) {
+    let accessToken = (req.body && req.body[this._accessTokenField]) || (req.query && req.query[this._accessTokenField]);
+    let refreshToken = (req.body && req.body[this._refreshTokenField]) || (req.query && req.query[this._refreshTokenField]);
+
+    if (!accessToken) return this.fail({message: `You should provide ${this._accessTokenField}`});
+
+    this._loadUserProfile(accessToken, (error, profile) => {
+      if (error) return this.error(error);
+
+      const verified = (error, user, info) => {
+        if (error) return this.error(error);
+        if (!user) return this.fail(info);
+
+        return this.success(user, info);
+      };
+
+      if (this._passReqToCallback) {
+        this._verify(req, accessToken, refreshToken, profile, verified);
+      } else {
+        this._verify(accessToken, refreshToken, profile, verified);
+      }
+    });
+  }
+
+  /**
+   * Parse user profile
+   * @param {String} accessToken Twitch OAuth2 access token
+   * @param {Function} done
+   */
+  userProfile(accessToken, done) {
+    this._oauth2.get(this._profileURL, accessToken, (error, body, res) => {
+      if (error) {
+        try {
+          let errorJSON = JSON.parse(error.data);
+          return done(new InternalOAuthError(errorJSON.message, errorJSON.status));
+        } catch (_) {
+          return done(new InternalOAuthError('Failed to fetch user profile', error));
+        }
+      }
+
+      try {
+        let json = JSON.parse(body);
+        json['id'] = json._id;
+
+        let profile = {
+          provider: 'twitch',
+          id: json.id,
+          username: json.name || '',
+          displayName: json.display_name || '',
+          name: {
+            familyName: json.display_name ? json.display_name.split(' ', 2)[0] : '',
+            givenName: json.display_name ? json.display_name.split(' ', 2)[1] : ''
+          },
+          emails: [{value: json.email}],
+          photos: [],
+          _raw: body,
+          _json: json
+        };
+
+        return done(null, profile);
+      } catch (e) {
+        return done(e);
+      }
+    });
+  }
+}
