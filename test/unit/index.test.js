fd1943f1b6d204fd78f92e383b727cc50db32409
Eugene Obrezkov <ghaiklor@gmail.com>
2015-10-01 14:05:05 +0300

    Fix tests

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -182,8 +182,7 @@ describe('TwitchTokenStrategy:userProfile', () => {
     }, 'not a JSON', null));
 
     strategy.userProfile('accessToken', (error, profile) => {
-      assert.equal(error.message, 'MESSAGE');
-      assert.equal(error.oauthCode, 'CODE');
+      assert.equal(error.message, 'Failed to fetch user profile');
       assert.equal(typeof profile, 'undefined');
       done();
     });
